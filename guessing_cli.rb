e812c3630d14dd8e54d33220003045543d6cd94a
Robert Tate <btate712@gmail.com>
2019-03-04 01:50:32 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   user_input = 0
-  target = rand(5).ceil + 1
+  target = 1 + rand(6)
   while user_input != "exit"
     puts "Guess a number between 1 and 6."
     user_input = gets.chomp
